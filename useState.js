b2f5003df7dde973c658c3e60d404b4c15a0def9
Kyril Jardinico <Kyril.Jay@gmail.com>
2021-08-18 23:05:10 -0400

    Update useState.js

--- useState.js
+++ useState.js
@@ -1,1 +1,19 @@
-/* Use state */
+import React, { useState } from 'react';
+
+function ClickTracker() {
+  const [clicks, setClicks] = useState(0);
+
+  function increment() {
+    setSteps(prevState => prevState + 1);
+  }
+
+  return (
+    <div>
+      You have clicked {steps} times!
+      <br />
+      <button onClick={increment}>
+        Click
+      </button>
+    </div>
+  );
+}
