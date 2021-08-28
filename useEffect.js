6ba4ee1b2a615f84efb0b593ca04b051e2add7d4
Kyril Jardinico <Kyril.Jay@gmail.com>
2021-08-27 23:50:51 -0400

    Update useEffect.js

--- useEffect.js
+++ useEffect.js
@@ -1,1 +1,13 @@
-
+import React,{useState, useEffect} from "react";
+const Clicker = () => {
+    const [click,setClick]=useState(0);
+    useEffect(() => {
+        document.title=`Number of clicks: ${click}`
+    },[]);
+    return (
+        <div>
+            <button onClick={()=>setClick(click+1)}>Click {click}</button>
+         </div>
+    )
+}
+export default Clicker;
