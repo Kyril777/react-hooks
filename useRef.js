c63a72377a26b330f6a410eeb9b8fe62637075ed
Kyril Jardinico <Kyril.Jay@gmail.com>
2021-08-31 23:58:33 -0400

    Update useRef.js

--- useRef.js
+++ useRef.js
@@ -1,1 +1,18 @@
 /* UseRef */
+
+import { useRef, useEffect } from 'react';
+
+function AccessElement() {
+  const elementRef = useRef();
+
+   useEffect(() => {
+    const divElement = elementRef.current;
+    console.log(divElement); 
+  }, []);
+
+  return (
+    <div ref={elementRef}>
+      Element
+    </div>
+  );
+}
