34e562beade99f52c3036ee9277cf2f612ed4134
Kyril Jardinico <Kyril.Jay@gmail.com>
2021-10-03 23:47:31 -0400

    Update useMemo.js

--- useMemo.js
+++ useMemo.js
@@ -1,1 +1,9 @@
+/* useMemo. */
 
+const Items = React.useMemo(() =>
+  listOfItems.map(item => ({
+    ...item,
+    itemProp1: exorbitantFunction(props.first),
+    itemProp2: anotherExorbitantFunction(props.second)
+  })), [listOfItems]
+)
