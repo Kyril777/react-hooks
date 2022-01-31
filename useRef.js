4d626434dc93ac5d12958f970704fdd383fe51c7
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-01-30 23:47:08 -0500

    Update useRef.js

--- useRef.js
+++ useRef.js
@@ -16,3 +16,35 @@ function AccessElement() {
     </div>
   );
 }
+
+
+/* useRef for render count.  Source: w3schools.com */
+
+import { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  
+  // Start the count in zero.
+  const [inputValue, setInputValue] = useState("");
+  const count = useRef(0);
+
+  // useEffect to add the county by 1.
+  useEffect(() => {
+    count.current = count.current + 1;
+  });
+
+   // Update when there is a change in input.
+  return (
+    <>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <h1>Render Count: {count.current}</h1>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
