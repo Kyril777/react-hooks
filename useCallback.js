e9793d63ac2accb9e4a25e86b1a5bb59683a6036
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-02-05 23:57:10 -0500

    Update useCallback.js

--- useCallback.js
+++ useCallback.js
@@ -7,6 +7,13 @@ function Component() {
   const click = useCallback(() => {
     console.log('Clicked!');
   }, []);
+}
 
-
+/* Source: Dmitri Pavlutin. */
+import React, { useCallback } from 'react';
+function MyComponent() {
+  const handleClick = useCallback(() => {
+    // handle the click event
+  }, []);
+  return <MyChild onClick={handleClick} />;
 }
