febfee54f7a317ef84c9a80810abef10b3d9c911
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-02-07 23:41:31 -0500

    Update useState.js

--- useState.js
+++ useState.js
@@ -1,6 +1,24 @@
+/* useState for basic clicktracker. */
 import React, { useState } from 'react';
 
-function ClickTracker() {
+function ClickTracker1() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+
+/* useState for tracking clicks. */
+import React, { useState } from 'react';
+
+function ClickTracker2() {
   const [clicks, setClicks] = useState(0);
 
   function increment() {
