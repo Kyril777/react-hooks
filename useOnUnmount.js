20717ba15471430992640bd80cb834463b75f40e
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-05-24 20:34:48 -0400

    Update useOnUnmount.js

--- useOnUnmount.js
+++ useOnUnmount.js
@@ -1,4 +1,20 @@
+// useOnUnmount hook to run code when the component unmounts.
+
 const useOnUnmount = onUnmount =>
   useEffect(() => {
     return () => onUnmount && onUnmount();
   }, []);
+
+
+import React from "react";
+import { useOnUnmount } from "react-hookedup";
+
+export default function App() {
+  useOnUnmount(() => console.log("unmount"));
+
+  return (
+    <div>
+      <p>hello world</p>
+    </div>
+  );
+}
