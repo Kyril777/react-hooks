632efdded07b4e38b3e4e16cc76ccc50d21d408c
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-06-11 23:32:56 -0400

    Update useOnMount.js

--- useOnMount.js
+++ useOnMount.js
@@ -1,4 +1,20 @@
+/* useOnMount runs code when the component is mounting */
+
 const useOnMount = onMount =>
   useEffect(() => {
     onMount && onMount();
   }, []);
+
+
+import React from "react";
+import { useOnMount } from "react-hookedup";
+
+export default function App() {
+  useOnMount(() => console.log("mounted"));
+
+  return (
+    <div>
+      <p>Hello World!</p>
+    </div>
+  );
+}
