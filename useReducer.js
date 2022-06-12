f40a98a0f27a2b7247aff30219451ee08875233c
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-06-12 18:13:33 -0400

    Update useReducer.js

--- useReducer.js
+++ useReducer.js
@@ -1,5 +1,4 @@
 /* Basic syntax for click application. */
-
 import { useReducer } from 'react';
 function Component() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -10,5 +9,32 @@ function Component() {
     <button onClick={() => dispatch(action)}>
       Click me
     </button>
+  );
+}
+
+
+/* Increment and decrement function. */
+const initialCount = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "reset":
+      return initialCount;
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+  }
+}
+
+function counter() {
+  const [state, dispatch] = useReducer(reducer, initialCount);
+  return (
+    <>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+    </>
   );
 }
